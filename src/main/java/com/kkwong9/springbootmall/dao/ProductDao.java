454e4a6d281d4178d15bcd068f67709c3b79ffcb
Kkwong-9 <mcwong9716@gmail.com>
2025-05-01 23:13:52 +0800

    商品功能 - 查詢商品列表 - 基礎實作

--- src/main/java/com/kkwong9/springbootmall/dao/ProductDao.java
+++ src/main/java/com/kkwong9/springbootmall/dao/ProductDao.java
@@ -3,10 +3,13 @@ package com.kkwong9.springbootmall.dao;
 import com.kkwong9.springbootmall.dto.ProductRequest;
 import com.kkwong9.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
     void deleteProductById(Integer productId);
+    List<Product> getProducts();
 }
--- src/main/java/com/kkwong9/springbootmall/service/ProductService.java
+++ src/main/java/com/kkwong9/springbootmall/service/ProductService.java
@@ -3,9 +3,12 @@ package com.kkwong9.springbootmall.service;
 import com.kkwong9.springbootmall.dto.ProductRequest;
 import com.kkwong9.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduce(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
+    List<Product> getProducts();
 }
